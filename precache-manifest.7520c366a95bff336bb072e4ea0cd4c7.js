74b44c0d6b71855d07cd5f4c4584d0a004088b56
leotubarao <l.tubarao.santos@gmail.com>
2019-06-23 15:10:41 -0300

    Updates

---
+++ precache-manifest.7520c366a95bff336bb072e4ea0cd4c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbe85421be4fc1b020584b22503d274",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e58d973f9ab79df06268",
+    "url": "/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "e58d973f9ab79df06268",
+    "url": "/static/js/2.fd5b0d60.chunk.js"
+  },
+  {
+    "revision": "589e26d5e71957635bdf",
+    "url": "/static/js/main.4e859293.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
