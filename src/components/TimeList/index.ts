9309009e2c5602e91419582f8624bb11b6ec96be
Leônidas Santos <oi@ltco.com.br>
2025-08-29 23:00:31 -0300

    feat: create TimeList component root

---
+++ src/components/TimeList/index.ts
@@ -0,0 +1,11 @@
+import { TimeContainer } from './Container';
+import { TimeEmpty } from './Empty';
+import { TimeForm } from './Form';
+import { TimeTableList } from './Table';
+
+export const TimeList = {
+  Root: TimeContainer,
+  Form: TimeForm,
+  Table: TimeTableList,
+  Empty: TimeEmpty,
+}
