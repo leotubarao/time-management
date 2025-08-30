97ec2c7da45da5f41b0f5dd98545cbe0e1a56b44
Leônidas Santos <oi@ltco.com.br>
2025-08-29 22:59:25 -0300

    feat: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header: React.FC = () => (
+  <section className="max-w-[1600px] mx-auto mb-8 bg-zinc-800 lg:grid lg:place-content-center rounded-lg">
+    <div className="mx-auto px-4 py-12 sm:py-18 lg:py-24">
+      <h1 className="text-center text-3xl sm:text-4xl md:text-5xl font-bold text-white text-balance">
+        Time Management
+      </h1>
+    </div>
+  </section>
+)
